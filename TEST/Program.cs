c348ace6d445e7817f98a20060de321d1f08b759
ericadonis <ericadonisforever@gmail.com>
2023-06-24 20:50:38 +0900

    first commit

---
+++ TEST/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+namespace TEST
+{
+    class Program
+    {
+        static int Main(String[] args)
+        {
+            Console.WriteLine("hello world! ABC");
+            //foreach (var s in args)
+            //{
+                
+            //    Console.WriteLine(s);
+            //}
+
+            MessageBox.Show("DotNet App Running Successfully!", "HostingCLR");
+            return 0;
+        }
+    }
+}
